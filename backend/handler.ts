2b0a815e855a29c3552e45c2383e977804e7d186
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-03-12 02:25:27 +0300

    chore: clear git cache

---
+++ backend/handler.ts
@@ -0,0 +1,15 @@
+"user strict";
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go serverless!!!! #thesmartcoder",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
