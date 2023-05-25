360771c7b07a7fce4318f6cc07c182f5f46fc0eb
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-05-25 19:14:19 +0300

    chore: uupdate project data and business layer

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -1,35 +1,35 @@
-import { TodosAccess } from "./todosAcess";
+import { TodosAccess } from "../dataLayer/todosAcess";
 import { AttachmentUtils } from "../helpers/attachmentUtils";
 import { TodoItem } from "../models/TodoItem";
 import { CreateTodoRequest } from "../requests/CreateTodoRequest";
-import { UpdateTodoRequest } from "../requests/UpdateTodoRequest";
+// import { UpdateTodoRequest } from "../requests/UpdateTodoRequest";
 import { createLogger } from "../utils/logger";
 import * as uuid from "uuid";
-import * as createError from "http-errors";
+// import * as createError from "http-errors";
 
 // TODO: Implement businessLogic
-const logger = createLogger('TodosAccess')
-const attachmentUtils = new AttachmentUtils()
-const todosAccess = new TodosAccess()
+const logger = createLogger("TodosAccess");
+const attachmentUtils = new AttachmentUtils();
+const todosAccess = new TodosAccess();
 
 // write create todo function
 export async function createTodo(
-    newTodo: CreateTodoRequest,
-    userId: string
-): Promise<TodoItem>{
-    logger.info('Create todo function called')
+  newTodo: CreateTodoRequest,
+  userId: string
+): Promise<TodoItem> {
+  logger.info("Create todo function called");
 
-    const todoId = uuid.v4()
-    const createdAt = new Date().toISOString()
-    const s3AttachmentUrl = attachmentUtils.getAttachmentUrl(todoId)
-    const newItem = {
-        userId,
-        todoId,
-        createdAt,
-        done: false,
-        attachmentUrl: s3AttachmentUrl,
-        ...newTodo
-    }
+  const todoId = uuid.v4();
+  const createdAt = new Date().toISOString();
+  const s3AttachmentUrl = attachmentUtils.getAttachmentUrl(todoId);
+  const newItem = {
+    userId,
+    todoId,
+    createdAt,
+    done: false,
+    attachmentUrl: s3AttachmentUrl,
+    ...newTodo,
+  };
 
-    return await todosAccess.createTodoItem(newItem)
+  return await todosAccess.createTodoItem(newItem);
 }
--- backend/src/dataLayer/todosAcess.ts
+++ backend/src/dataLayer/todosAcess.ts
@@ -1,12 +1,53 @@
-import * as AWS from 'aws-sdk'
-import * as AWSXRay from 'aws-xray-sdk'
-import { DocumentClient } from 'aws-sdk/clients/dynamodb'
-import { createLogger } from '../utils/logger'
-import { TodoItem } from '../models/TodoItem'
-import { TodoUpdate } from '../models/TodoUpdate';
+import * as AWS from "aws-sdk";
+import * as AWSXRay from "aws-xray-sdk";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { createLogger } from "../utils/logger";
+import { TodoItem } from "../models/TodoItem";
+// import { TodoUpdate } from "../models/TodoUpdate";
 
-const XAWS = AWSXRay.captureAWS(AWS)
+const XAWS = AWSXRay.captureAWS(AWS);
 
-const logger = createLogger('TodosAccess')
+const logger = createLogger("TodosAccess");
 
 // TODO: Implement the dataLayer logic
+export class TodosAccess {
+  constructor(
+    private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+    private readonly todosTable = process.env.TODOS_TABLE,
+    private readonly todosIndex = process.env.INDEX_NAME
+  ) {}
+
+  async getAllTodos(userId: string): Promise<TodoItem[]> {
+    logger.info("Get all todos function called");
+
+    const result = await this.docClient.query({
+      TableName: this.todosTable,
+      IndexName: this.todosIndex,
+      KeyConditionExpression: "userId = :userId",
+      ExpressionAttributeValues: {
+        userId: userId,
+      },
+    }).promise();
+
+    const items = result.Items
+    return items as TodoItem[]
+  }
+
+  async createTodoItem(todoItem: TodoItem): Promise<TodoItem>{
+    logger.info('Create todo item function called')
+
+    const result = await this.docClient.put({
+        TableName: this.todosTable,
+        Item: todoItem
+    }).promise()
+
+    logger.info('Todo item created', result)
+    return todoItem as TodoItem
+  }
+
+//   async updateTodoItem(
+//     todoId: string,
+//     userId: string,
+//     TodoUpdate: TodoUpdate
+//   )
+}
